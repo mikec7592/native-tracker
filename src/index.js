f60e67ae516f029759a2e71d2730e8bd2c4068f8
michael clark <michaelclark@michaels-Air.fios-router.home>
2022-03-15 12:28:39 -0400

    added foundation for JWTs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes')
+const requireAuth = require('./middleware/requireAuth');
 
 const app = express();
 
@@ -20,8 +21,8 @@ mongoose.connection.on('error', (err) => {
     console.log('Error connecting to Mongo', err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hi there!');
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email : ${req.user.email}`);
 });
 
 app.listen(3000, () => {
